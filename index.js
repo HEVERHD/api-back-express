ffe78289d47ec8a8a2f706829af6d58c8c3a10bc
HEVERHD <hever11.hdgd@gmail.com>
2022-04-29 12:19:38 -0500

    Instalpack

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
-
+//configuraciones de express
 app.set("port", 3000);
+
+//middlewares para acceder a los datos estaticos
+app.use(express.static(path.join(__dirname, "public")));
+
+
+//rutas
+app.get("/", (req, res) => {
+    res.send("Hola mundo");
+});
 
 app.listen(app.get("port"), () => {
   console.log(`App is running on port ${app.get("port")}`);
